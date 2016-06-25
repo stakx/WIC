5ade38f3883cf43d910447a91fa9102c44c8f208
stakx <stakx@eml.cc>
2016-06-25 23:19:25 +0200

    Add alternative safe interface `IWICBitmapFrameEncodeSafe`
    Any `IWICBitmapFrameEncode` (whic contains an `unsafe` method
    `WriteSource`) can be cast to `IWICBitmapFrameEncodeSafe`, which
    contains a safe version of that method.

---
+++ stakx.WIC.Interop/Interfaces/IWICBitmapFrameEncodeSafe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace stakx.WIC.Interop
+{
+    [ComImport]
+    [Guid(IID.IWICBitmapFrameEncode)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IWICBitmapFrameEncodeSafe
+    {
+        void Initialize(
+            [In] IPropertyBag2 pIEncoderOptions);
+
+        void SetSize(
+            [In] int uiWidth,
+            [In] int uiHeight);
+
+        void SetResolution(
+            [In] double dpiX,
+            [In] double dpiY);
+
+        void SetPixelFormat(
+            [In, Out] ref Guid pPixelFormat);
+
+        void SetColorContexts(
+            [In] int cCount,
+            [In, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.Interface, SizeParamIndex = 0)] IWICColorContext[] ppIColorContext);
+
+        void SetPalette(
+            [In] IWICPalette pIPalette);
+
+        void SetThumbnail(
+            [In] IWICBitmapSource pIThumbnail);
+
+        void WritePixels(
+            [In] int lineCount,
+            [In] int cbStride,
+            [In] int cbBufferSize,
+            [In, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U1, SizeParamIndex = 2)] byte[] pbPixels);
+
+        void WriteSource(
+            [In] IWICBitmapSource pIBitmapSource,
+            [In] IntPtr prc);
+
+        void Commit();
+
+        void GetMetadataQueryWriter(
+            [Out] out IWICMetadataQueryWriter ppIMetadataQueryWriter);
+    }
+}
