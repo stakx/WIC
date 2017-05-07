e4ff7671ec0ce2fe35d24ab053922212c9357b19
stakx <stakx@eml.cc>
2017-05-07 14:01:03 +0200

    Convert project files to new VS2017 format
    The new MSBuild-based project file format will allow us to easily
    build a NuGet package without having to maintain a separate `.nuspec`
    so let's upgrade.

--- stakx.WIC/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General information about this assembly.
-[assembly: AssemblyTitle("stakx.WIC")]
-[assembly: AssemblyDescription("Extension methods and abstractions that make it easier to work with the types defined in `stakx.WIC.Interop`.")]
-
-[assembly: Guid("118f10fe-f227-4b22-aea5-6677c105f73a")]
